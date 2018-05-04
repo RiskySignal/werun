e666aefd816a6f24b0860ababc383d1c4c9a196c
hellochicken <543457946@qq.com>
2018-05-04 11:28:01 +0800

    添加登录界面
    添加了登录界面，并且打包生成，进行测试

--- src/client/components/login-box/login-box.jsx
+++ src/client/components/login-box/login-box.jsx
@@ -7,8 +7,13 @@
 * @Last Modified time: 2018-01-17
 */
 require("./login-box.scss");
-const Jumbotron = Bootstrap.Jumbotron;
 const Panel = Bootstrap.Panel;
+const image_QQ = require("./QQ.svg");
+const image_wechart = require("./wechart.svg");
+const FormGroup = Bootstrap.FormGroup;
+const FormControl = Bootstrap.FormControl;
+const Form = Bootstrap.Form;
+const Button = Bootstrap.Button;
 
 class LoginBox extends React.Component {
     constructor(props, context) {
@@ -32,26 +37,57 @@ class LoginBox extends React.Component {
 
     render() {
         return (
-            <div className="container" id="login-box">
-                <Jumbotron>
+            <div className="container-fluid" id="login-box">
+                <div
+                    className="container col-lg-6 offset-lg-3 col-md-8 offset-md-2 col-sm-10 offset-sm-1 col-xs-12 offset-xs-0"
+                    id="login-content">
                     <Panel bsStyle="primary">
-                        <Panel.Heading>
-                            <Panel.Title componentClass="h3">Panel heading</Panel.Title>
-                        </Panel.Heading>
-                        <Panel.Body>Panel content</Panel.Body>
-                    </Panel>
-                    <Panel bsStyle="primary">
-                        <Panel.Heading>
-                            <span>Log in with</span>
-                        </Panel.Heading>
+                        <Panel.Heading className="text-center">Log in with</Panel.Heading>
                         <Panel.Body>
-                            github
+                            <div className="container">
+                                <div className="account-socials">
+                                    <div className="account-socials-row">
+                                        <a href="#" className="account-social">
+                                            <div className="image">
+                                                <img src={image_QQ} alt="qq-登录"/>
+                                            </div>
+                                            <div className="text-center">腾讯qq</div>
+                                        </a>
+                                        <a href="#" className="account-social">
+                                            <div className="image">
+                                                <img src={image_wechart} alt="wechart-登录"/>
+                                            </div>
+                                            <div className="text-center">微信</div>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div className="dropdown-divider">OR</div>
+                                <Form
+                                    className="col-xs-12 col-lg-9 col-sm-11 col-md-10 col-center-block custom-input-form">
+                                    <FormGroup>
+                                        <FormControl type="email" placeholder="account"/>
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <FormControl type="password" placeholder="password"/>
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <span className="login-error-text text-danger hidden">账户密码不正确</span>
+                                    </FormGroup>
+                                    <FormGroup className="text-center forget-group">
+                                        <Button id="login" className="btn-primary">Log in</Button>
+                                        <a href="#" id="forget-link">Forgot your password?</a>
+                                    </FormGroup>
+                                    <FormGroup className="text-center">
+                                        <b>Don't have an account?</b>
+                                        <a href="#" id="signup-link">Sign up</a>
+                                    </FormGroup>
+                                </Form>
+                            </div>
                         </Panel.Body>
-                        <Panel.Footer>
-                            <span>© Copyright2018, WeRun Club</span>
-                        </Panel.Footer>
+                        <Panel.Footer className="text-center">© Copyright 2018, WeRun Club. All rights
+                            reserved.</Panel.Footer>
                     </Panel>
-                </Jumbotron>
+                </div>
             </div>
         );
     }

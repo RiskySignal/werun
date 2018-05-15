3e3b51bae01db0c792f11902693fb20734b14210
Neeze <543457946@qq.com>
2018-05-15 20:16:51 +0800

    添加成员展示界面
    添加成员展示界面

--- dist/web/js/animate.js
+++ dist/web/js/animate.js
@@ -1,3 +1,3 @@
 /*! 哈尔滨工业大学(威海) 软件学院 WeRun 俱乐部 */
-webpackJsonp([5],{392:function(n,c){}},[392]);
+webpackJsonp([5],{390:function(n,c){}},[390]);
 //# sourceMappingURL=animate.js.map
--- src/client/page/member/member.jsx
+++ src/client/page/member/member.jsx
@@ -18,56 +18,64 @@ class MemberPage extends Component {
                 <NavigationBar/>
                 <div className="container">
                     <div className="row">
-                        <main className="main-content col-md-10">
+                        <main className="main-content col-md-9">
                             <section>
                                 <h4 className="section-title">导师队伍</h4>
-                                <ul className="list">
-                                    <li className="col-md-4 item">
-                                        <img src={Mayun} alt="mayun"/>
-                                        <div className="captionWrapper">
-                                            <div className="heading">马云</div>
-                                            <div className="text">荣誉客座老师</div>
+                                <ul className="list clearfix">
+                                    <li className="col-md-4 col-sm-6">
+                                        <div className={'item'}>
+                                            <img src={Mayun} alt="mayun"/>
+                                            <div className="captionWrapper">
+                                                <div className="heading">马云</div>
+                                                <div className="text">荣誉客座老师</div>
+                                            </div>
                                         </div>
                                     </li>
-                                    <li className="col-md-4">
-                                        <img src={Mayun} alt="mayun"/>
-                                        <div className="captionWrapper">
-                                            <div className="heading">马云</div>
-                                            <div className="text">荣誉客座老师</div>
+                                    <li className="col-md-4 col-sm-6">
+                                        <div className={'item'}>
+                                            <img src={Mayun} alt="mayun"/>
+                                            <div className="captionWrapper">
+                                                <div className="heading">马云</div>
+                                                <div className="text">荣誉客座老师</div>
+                                            </div>
                                         </div>
                                     </li>
-                                    <li className="col-md-4">
-                                        <img src={Mayun} alt="mayun"/>
-                                        <div className="captionWrapper">
-                                            <div className="heading">马云</div>
-                                            <div className="text">荣誉客座老师</div>
+                                    <li className="col-md-4 col-sm-6">
+                                        <div className={'item'}>
+                                            <img src={Mayun} alt="mayun"/>
+                                            <div className="captionWrapper">
+                                                <div className="heading">马云</div>
+                                                <div className="text">荣誉客座老师</div>
+                                            </div>
                                         </div>
                                     </li>
-                                    <li className="col-md-4">
-                                        <img src={Mayun} alt="mayun"/>
-                                        <div className="captionWrapper">
-                                            <div className="heading">马云</div>
-                                            <div className="text">荣誉客座老师</div>
+                                    <li className="col-md-4 col-sm-6">
+                                        <div className={'item'}>
+                                            <img src={Mayun} alt="mayun"/>
+                                            <div className="captionWrapper">
+                                                <div className="heading">马云</div>
+                                                <div className="text">荣誉客座老师</div>
+                                            </div>
                                         </div>
                                     </li>
                                 </ul>
                             </section>
                         </main>
-                        <aside className="col-md-2 side-bar">
+                        <aside className="col-md-3 side-bar hidden-sm hidden-xs">
                             <div className="widget list-bar">
                                 <h4 className="title">俱乐部成员</h4>
                                 <ul className="list">
                                     <li>
-                                        <a href="">2017</a>
+                                        <p className="title">指导老师</p>
                                     </li>
                                     <li>
-                                        <a href="">2017</a>
+                                        <p className="title">2018界成员</p>
                                     </li>
                                     <li>
-                                        <a href="">2017</a>
+                                        <p className="title">2017界成员</p>
                                     </li>
                                     <li>
-                                        <a href="">2017</a>
+                                        <p className="title">2016界成员</p>
                                     </li>
                                 </ul>
                             </div>
